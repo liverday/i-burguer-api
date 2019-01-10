f227c077cb74f6d3f0460e3d8b25f80292d69dbc
liverday <vitor.medeiro10@gmail.com>
2019-01-10 01:38:20 -0200

    adicionando body parser e alterando limite de tamanho

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
 import { cors } from './cors-middleware'
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
   app.use(cors);
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
