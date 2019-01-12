39df4bd1ebca0a64aa1d84670ba9239c594b26c4
liverday <vitor.medeiro10@gmail.com>
2019-01-12 02:47:24 -0200

    alterando ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,12 @@
 const env = require('dotenv')
 env.config()
 
-const isProd = process.env.NODE_ENV === 'production'
-const entitiesExtension =  isProd ? 'js' : 'ts'
 const migrationsDir = isProd ? 'dist/migration/*.js' : 'src/migration/*.ts'
 
 module.exports = {
   type: "mysql",
   entities: [
-    __dirname + '/**/*.entity.' + entitiesExtension
+    __dirname + '/**/*.entity.ts'
   ],
   url: process.env.CLEARDB_DATABASE_URL,
   synchronize: false,
