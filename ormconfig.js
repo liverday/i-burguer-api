86da27cc9b99957b59ec4e782213927955415598
liverday <vitor.medeiro10@gmail.com>
2019-01-12 02:48:20 -0200

    fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,6 @@
 const env = require('dotenv')
 env.config()
 
-const migrationsDir = isProd ? 'dist/migration/*.js' : 'src/migration/*.ts'
-
 module.exports = {
   type: "mysql",
   entities: [
@@ -10,8 +8,4 @@ module.exports = {
   ],
   url: process.env.CLEARDB_DATABASE_URL,
   synchronize: false,
-  migrations: [migrationsDir],
-  cli: {
-    migrationsDir: 'src/migration'
-  }
 }
